1a9ab6b1750f4dd78225145cea9a7401ce5958ab
skvoruha <skvoruha@mail.ru>
2022-02-04 15:32:55 +0300

    fix errors ars string and number

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-const arg = prompt()
+const arg = +prompt("Аргумент не строчный +prompt")
+const argStr = prompt("Аргумент строчный prompt")
 
 function getArgument(argument){
-  if (typeof(argument) != 'string'){
+  if (typeof(argument) != "string"){
     console.log("Уведомление, аргумент : \n" + argument + "\n Не строка" );
-  }
-  argument = argument.trim();
-  if(argument.length > 30) {
-    let string = argument.slice(0, 30);
-        console.log(string + '...');
-    } else {
-        console.log(argument);
+  } else {
+    argument = argument.trim();
+    if(argument.length > 30) {
+      let string = argument.slice(0, 30);
+          console.log(string + '...');
+      } else {
+          console.log(argument);
+      }
     }
 }
-console.log(getArgument(arg));
+getArgument(arg)
+getArgument(argStr)
+
 
 
