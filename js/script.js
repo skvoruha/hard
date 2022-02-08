f76c0178b4c8341ea534928bee15dfdf8c41823d
skvoruha <skvoruha@mail.ru>
2022-02-08 11:48:06 +0300

    add check trim()

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 
-const isNumber = function(num){
-   return /^-?[\d.]+(?:e-?\d+)?$/.test(num);
-}
+// const isNumber = function(num){
+//    return /^-?[\d.]+(?:e-?\d+)?$/.test(num);
+// }
 
-screenPrice = prompt("Сколько будет стоить данная работа?");
-while(!isNumber(screenPrice)){
-  screenPrice = +prompt("Сколько будет стоить данная работа?");
-}
+const isNumber = function (num) {
+  return (typeof num == 'string' || typeof num == 'number') && !isNaN(num - 0) && num !== '';
+};
+
+do {
+  screenPrice = prompt("Сколько будет стоить данная работа?").trim();
+} while(!isNumber(screenPrice))
+
+console.log(screenPrice);
+console.log(screenPrice);
+console.log(typeof screenPrice);
