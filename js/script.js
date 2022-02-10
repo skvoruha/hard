837cc03cf540957de525f1ec3abfe19447e6cc2a
skvoruha <skvoruha@mail.ru>
2022-02-10 20:06:36 +0300

    create function on day, style italic - bold

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
+  'Пятница', 'Суббота', 'Воскресенье'];
+
+const todayWeek = function () {
+  let numDay = new Date().getDay();
+  // у нас воскресенье последний день недели, а не первый
+  return (numDay) ? numDay - 1 : 6;
+};
+
+
+week.forEach((element, index) => {
+  console.log(index);
+  const body = document.querySelector('body');
+  const current = todayWeek();
+  body.innerHTML +="<div class='day'>" + element + "</div>";
+  // выходные
+    if (index > 4) {
+      document.getElementsByClassName('day')[index].style.fontStyle = "italic";
+    }
+    // сегодняшний день
+    if (index === current) {
+      document.getElementsByClassName('day')[index].style.fontWeight = "900";
+    }
+});
+
+
+// const allWeekDay = function () {
+//   const body = document.querySelector('body');
+//   const current = todayWeek();
+
+//   let output = '';
+
+//   week.forEach( => {
+//     let dayWeek = day;
+
+//     // выхи
+//     if (index > 4) {
+//       dayWeek = "<i>" + dayWeek + "</i>"
+//     }
+//     // сегодняшний день
+//     if (index === current) {
+//       dayWeek = "<b>" + dayWeek + "</b>"
+//     }
+//     // ставим тег br для переноса строки
+//     output += dayWeek + '<br />';
+//   });
+//   body.innerHTML = output;
+// };
+
+// allWeekDay();
