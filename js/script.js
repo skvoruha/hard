27a4b52dd13a1e20b94cae2a335a7516a301b243
skvoruha <skvoruha@mail.ru>
2022-02-10 20:11:06 +0300

    fix check on lesson07

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+// "use strict";
+
+// const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
+//   'Пятница', 'Суббота', 'Воскресенье'];
+
+// const todayWeek = function () {
+//   let numDay = new Date().getDay();
+//   // у нас воскресенье последний день недели, а не первый
+//   return (numDay) ? numDay - 1 : 6;
+// };
+
+
+// const allWeekDay = function () {
+//   const body = document.querySelector('body');
+//   const current = todayWeek();
+
+//   let output = '';
+
+//   week.forEach((day, index) => {
+//     let dayWeek = day;
+
+//     // выхи
+//     if (index > 4) {
+//       dayWeek = "<i>" + dayWeek + "</i>"
+//     }
+//     // сегодняшний день
+//     if (index === current) {
+//       dayWeek = "<dev><b>" + dayWeek + "</b></dev>"
+//     }
+//     // ставим тег br для переноса строки
+//     output += dayWeek + '<br />';
+//   });
+//   body.innerHTML = output;
+// };
+
+// allWeekDay();
+
 "use strict";
 
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
@@ -10,27 +47,18 @@ const todayWeek = function () {
 };
 
 
-const allWeekDay = function () {
+week.forEach((element, index) => {
+  console.log(index);
   const body = document.querySelector('body');
   const current = todayWeek();
-
-  let output = '';
-
-  week.forEach((day, index) => {
-    let dayWeek = day;
-
-    // выхи
+  body.innerHTML +="<div class='day'>" + element + "</div>";
+  // выходные
     if (index > 4) {
-      dayWeek = "<i>" + dayWeek + "</i>"
+      document.getElementsByClassName('day')[index].style.fontStyle = "italic";
     }
     // сегодняшний день
     if (index === current) {
-      dayWeek = "<dev><b>" + dayWeek + "</b></dev>"
+      document.getElementsByClassName('day')[index].style.fontWeight = "Bold";
     }
-    // ставим тег br для переноса строки
-    output += dayWeek + '<br />';
-  });
-  body.innerHTML = output;
-};
+});
 
-allWeekDay();
