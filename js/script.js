386a80c2d130607d4f5643938176d333a035c614
skvoruha <skvoruha@mail.ru>
2022-02-16 23:04:02 +0300

    fix setinterval

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const body = document.querySelector('body');
+
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
   'Пятница', 'Суббота', 'Воскресенье'];
 
@@ -8,15 +10,18 @@ const todayWeek = function () {
   // у нас воскресенье последний день недели, а не первый
   return (numDay) ? numDay - 1 : 6;
 };
-// переменная для получение часов минут секунд
-const dateTime = new Date();
-// переменный для задание б
-const nowTime = new Date().toLocaleTimeString();
-const nowDate = new Date().toLocaleDateString();
+
+
+
+
 
 let screenOutput = "";
+
 // функция окончаний
-const checkingTimeCompletion = function(){
+setInterval(function checkingTimeCompletion(){
+  let dateTime = new Date();
+  const nowTime = new Date().toLocaleTimeString();
+  const nowDate = new Date().toLocaleDateString();
   // Создаём объект с окончаниями
   const ending = {
     nameHourEnding : {
@@ -45,7 +50,7 @@ const checkingTimeCompletion = function(){
   const keys = Object.keys(ending)
   // перебор массива с названием ключей
   keys.forEach(element => {
-    screenOutput += checkNameTime(element)
+     screenOutput += checkNameTime(element)
     // console.log(element);
   });
 
@@ -59,26 +64,21 @@ const checkingTimeCompletion = function(){
     // вар a
     // вывод 1 час, 21 час 1 минута 1 секунда , всё что связано с окончаниеем 1 кромне 11
     if (time % 10 == 1 && time !== 11){Output = time + ending[nameTime][0] + ending[nameTime][3]
-      console.log(Output);
     // вывод с оконание  , 2 часа , 2 минуты , 2 секунды
   } else if (time % 10 > 1 && time % 10 < 5 ) {Output = time + ending[nameTime][0] + ending[nameTime][1]
-    console.log(Output);
     // вывод с оконание ов , 13 часов , 13 минут , 13 секунд 2 часа
-    } else if (time % 10 >= 5 || time % 10 == 0 || time == 11 ) {Output = time + ending[nameTime][0] + ending[nameTime][2]
-      console.log(Output);
+    } else if (time % 10 >= 5 || time % 10 == 0 || time == 11 )
+    {Output = time + ending[nameTime][0] + ending[nameTime][2]
     }
     return Output
     // Включить while
     // }while(time < 60)
   } // конец функции checkNameTime
 
-}
-checkingTimeCompletion()
+  body.innerHTML = "<div class='day'> Сегодня "+ week[todayWeek()] + " " + dateTime.getFullYear() + " года " + " " + screenOutput + " "  +  "</div>"
 
+  body.innerHTML +=  "<div class='day'>" + nowDate + " " + nowTime +  "</div>"
+  screenOutput = ""
 
-const body = document.querySelector('body');
-body.innerHTML = "<div class='day'> Сегодня "+ week[todayWeek()] + " " + dateTime.getFullYear() + " года " + " " + screenOutput + " "  +  "</div>"
-// body.innerHTML +="<div class='day'> Сегодня "+ week[todayWeek()] + " " +  date +" " + time.getHours() + " час " + time.getMinutes() + " минут  "+ time.getSeconds() + " секунды "  +"</div>";
-// document.querySelector('.day').style.fontWeight = "900";
-body.innerHTML +=  "<div class='day'>" + nowDate + " " + nowTime +  "</div>"
+},1000)
 
