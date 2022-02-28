022f4cce7421f90277d3378dd88928eaffff97d2
skvoruha <skvoruha@mail.ru>
2022-02-28 14:43:17 +0300

    РІС‹РїРѕР»РЅРµРЅР° Р·Р°РґС‡Р°

--- js/script.js
+++ js/script.js
@@ -1,86 +1,89 @@
-"use strict";
-
-const body = document.querySelector('body');
-
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
+// передаём дедлайн в функцию
+const timer = (deadline) => {
+  // запишем в перменная элменты со временм
+  const timerHours = document.getElementById('timer-hours')
+  const timerMinutes = document.getElementById('timer-minutes')
+  const timerSeconds = document.getElementById('timer-seconds')
+  const timerDays = document.getElementById('timer-days')
+  const timeNow = document.getElementById('timeNow')
+  // получаем пременную текщим времени суток (утро, день, вечер, ночь)
+  const timeDay = document.getElementById('timeDay')
+  // переменная с днями недели
+  const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг',
   'Пятница', 'Суббота', 'Воскресенье'];
 
-const todayWeek = function () {
-  let numDay = new Date().getDay();
-  // у нас воскресенье последний день недели, а не первый
-  return (numDay) ? numDay - 1 : 6;
-};
+  const todayWeek = function () {
+    let numDay = new Date().getDay();
+    // у нас воскресенье последний день недели, а не первый
+    return (numDay) ? numDay - 1 : 6;
+  };
 
+  const getTimeNow = () =>{
+    const time = new Date()
+    const hoursNow = +time.getHours()
+    timeNow.textContent = time.toLocaleTimeString('en')
+    if (hoursNow >= 0 && hoursNow <= 6) {
+      timeDay.textContent = 'Доброй Ночи'
+    } else if (hoursNow > 6 && hoursNow <= 12){
+      timeDay.textContent = 'Доброе Утро'
+    } else if (hoursNow > 12 && hoursNow <= 18){
+      timeDay.textContent = 'Добрый День'
+    } else if (hoursNow > 18 && hoursNow <= 24){
+      timeDay.textContent = 'Добрый Вечер'
+    }
+    setTimeout(getTimeNow, 1000)
+  }
 
+  const getTimeRemaining = () =>{
 
+    // получим в милисекундах время дедлайна
+    let dateStop = new Date(deadline).getTime()
+    // получим в милисекундах время сейчас
+    let dateNow = new Date().getTime()
+    // переменная с разницой во времени в милисек
+    // let timeRemaining = dateStop - dateNow
+    // Чтобы получить в секундах делм на 1000
+    let timeRemaining = (dateStop - dateNow) / 1000
+    // ПЕРЕМЕННАЯ КОЛИЧЕСВТВО ДНЕЙ возьмём расчёт часов
+    let days = Math.floor(timeRemaining / 60 / 60 / 24)
+    // чтобы получить час ынжно раздалить на 60 и еще на 60
+    // чтобы показывадло не 150 часов нужно разделить ещё на 24 и получить остаотк от деления
+    let hours = Math.floor((timeRemaining / 60 / 60) % 24)
+    //  чтоы получить минуты нужно раздлеить на 60
+    let minutes = Math.floor((timeRemaining / 60) % 60)
+    // округление числа до целочисленного и птом получаем остаток от деления
+    let seconds = Math.floor(timeRemaining % 60)
 
+    // даная функция будет возвращать объект
+    // если навзание ключа соответсвет названию переменной то можно убрать название ключа       days: days,
+    return {timeRemaining, days, hours, minutes,seconds }
+  }
+  const updateClock = () =>{
+    let getTime = getTimeRemaining()
 
-let screenOutput = "";
-
-// функция окончаний
-function checkingTimeCompletion(){
-  let dateTime = new Date();
-  const nowTime = new Date().toLocaleTimeString();
-  const nowDate = new Date().toLocaleDateString();
-  // Создаём объект с окончаниями
-  const ending = {
-    nameHourEnding : {
-        theTime: dateTime.getHours(),
-        0:' час',
-        1:'а ',
-        2:'ов ',
-        3:''
-    },
-    nameMinutesEnding : {
-        theTime: dateTime.getMinutes(),
-        0:' минут',
-        1:'ы ',
-        2:' ',
-        3:'а '
-    },
-    nameSecondsEnding : {
-        theTime: dateTime.getSeconds(),
-        0:' секунд',
-        1: 'ы ',
-        2: '',
-        3: 'а '
+    timerHours.textContent = String(getTime.hours).padStart(2,'0')
+    timerMinutes.textContent = String(getTime.minutes).padStart(2,'0')
+    timerSeconds.textContent = String(getTime.seconds).padStart(2,'0')
+    timerDays.textContent = getTime.days
+    if (getTime.timeRemaining > 0) {
+      setTimeout(updateClock, 1000)
+    } else {
+      timerHours.textContent = '00'
+      timerMinutes.textContent = '00'
+      timerSeconds.textContent = '00'
+      timerDays.textContent = 0
     }
   }
-  // присваиваем ключи объекта ending
-  const keys = Object.keys(ending)
-  // перебор массива с названием ключей
-  keys.forEach(element => {
-     screenOutput += checkNameTime(element)
-    // console.log(element);
-  });
 
+  // дата по делайну которую передаем в функцию
+  // getTime('27 February 2022')
+  // третим параметрм будет аргумеент передаваемый в функцию  getTimeRemaining('27 February 2022')
+  // setInterval(getTimeRemaining, 1000 , '27 February 2022')
+  updateClock()
 
-  function checkNameTime(nameTime) {
-    const time = ending[nameTime].theTime
-    // ДЛЯ ПРОВЕРКИ ЧИСАЛ ВКЛЮЧИТЬ do while в конце до 60 чисел
-    // do{
-    // const time = +prompt()
-    let Output
-    // вар a
-    // вывод 1 час, 21 час 1 минута 1 секунда , всё что связано с окончаниеем 1 кромне 11
-    if (time % 10 == 1 && time !== 11){Output = time + ending[nameTime][0] + ending[nameTime][3]
-    // вывод с оконание  , 2 часа , 2 минуты , 2 секунды
-  } else if (time % 10 > 1 && time % 10 < 5 ) {Output = time + ending[nameTime][0] + ending[nameTime][1]
-    // вывод с оконание ов , 13 часов , 13 минут , 13 секунд 2 часа
-    } else if (time % 10 >= 5 || time % 10 == 0 || time == 11 )
-    {Output = time + ending[nameTime][0] + ending[nameTime][2]
-    }
-    return Output
-    // Включить while
-    // }while(time < 60)
-  } // конец функции checkNameTime
-
-  body.innerHTML = "<div class='day'> Сегодня "+ week[todayWeek()] + " " + dateTime.getFullYear() + " года " + " " + screenOutput + " "  +  "</div>"
-
-  body.innerHTML +=  "<div class='day'>" + nowDate + " " + nowTime +  "</div>"
-  screenOutput = ""
-
+  getTimeNow()
 }
+// экспортируем функцию
 
-setInterval(checkingTimeCompletion,1000)
+timer('1 January 2023')
 
