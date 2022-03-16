90077e94466c7aba93732451511bffca188b797d
skvoruha <skvoruha@mail.ru>
2022-03-16 13:17:40 +0300

    completed leson26 hard

--- js/script.js
+++ js/script.js
@@ -1,37 +1,44 @@
 'use strict'
 
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const pass = document.getElementById('password')
+const getDataXml = (url) =>{
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset utf-8');
 
-const getData = (url) =>{
-  return fetch(url)
-    .then(response =>response.json())
-    .catch(() =>{
-      console.log("Произошла ошибка при получении данных");
-    })
+  try {
+    xhr.send();
+    if (xhr.status != 200) {
+      alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      console.log(JSON.parse(xhr.response))
+      return JSON.parse(xhr.response)
+    }
+  } catch(err) { // для отлова ошибок используем конструкцию try...catch вместо onerror
+    alert("Запрос не удался");
+  }
+}
+
+const sendDataXml = (url, data) =>{
+  // отправим данные
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+  xhr.setRequestHeader('Content-type', 'application/json; charset utf-8');
+
+  xhr.upload.onerror = function() {
+    alert(`Произошла ошибка во время отправки: ${xhr.status}`);
+  };
+
+  xhr.send(data);
+
+  xhr.onload = () =>{
+    console.log(JSON.parse(xhr.response))
+    return JSON.parse(xhr.response)
+  }
+
 }
 
 
+sendDataXml('https://jsonplaceholder.typicode.com/posts', getDataXml('db.json'))
 
-const sendData = (url, data) =>{
-  return fetch(url,{
-    method: 'POST',
-    body: data,
-    headers: {
-      'Content-type': 'application/json; charset utf-8',
-    },
-  })
-    .then(response =>response.json())
-    .catch(() =>{
-      console.log("Произошла ошибка при отправки данных");
-    })
 
-}
-// получаем данные из файла db.json
-getData('db.json').then(data =>{
-  console.log(data)
-  sendData('https://jsonplaceholder.typicode.com/posts',JSON.stringify(data)).then(data =>{
-      console.log(data);
-  })
-})
+
